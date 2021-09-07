fcb86bc531d2e48226e611038cc28d807c3cddbf
Tom Capers <tompcapers@gmail.com>
2021-09-07 12:30:57 -0700

    updated for bcrypt and added json seed file

--- tech-news-java-api/src/main/java/com/technews/controller/UserController.java
+++ tech-news-java-api/src/main/java/com/technews/controller/UserController.java
@@ -10,6 +10,7 @@ import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
